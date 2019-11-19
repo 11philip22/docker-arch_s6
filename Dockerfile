c551a933505b0b093887b88af4e360c073931162
11philip22 <philipwoldhek@gmail.com>
2019-11-19 22:06:24 +0100

    test

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,8 @@
 FROM archlinux/base
 
 COPY root/ /
-
-RUN pacman -Sy tar --noconfirm
 ADD https://github.com/just-containers/s6-overlay/releases/download/v1.21.8.0/s6-overlay-amd64.tar.gz /tmp/
-RUN gunzip -c /tmp/s6-overlay-amd64.tar.gz | tar -xf - -C /; \
-    rm -rf /tmp/*
 
-RUN bash /usr/local/bin/fix_bins.sh
-
-RUN pacman -Rs tar --noconfirm; \
-    pacman -Scc --noconfirm
-
-RUN mkdir -p /config \
-             /defaults 
-
-RUN useradd -u 1000 -U -d /config -s /sbin/nologin user
+RUN /usr/local/bin/install.sh
 
 ENTRYPOINT ["/init"]
---
+++ root/usr/local/bin/install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+echo -e "\e[32m[INFO] Unpacking s6 overlay \e[0m"
+pacman -Sy tar --noconfirm
+gunzip -c /tmp/s6-overlay-amd64.tar.gz | tar -xf - -C /
+rm -rf /tmp/*
+
+echo -e "\e[32m[INFO] Repairing symlink to bin \e[0m"
+if [ ! -L "/bin" ]; then
+  cd / || exit 1
+  tar cf - bin | ( cd /usr || exit 1; tar xfp - )
+  rm -rf -- bin/*
+  rmdir bin
+  ln -s /usr/bin bin
+fi
+
+echo -e "\e[32m[INFO] Cleanup build depandancies \e[0m"
+pacman -Rs tar --noconfirm
+pacman -Scc --noconfirm
+
+echo -e "\e[32m[INFO] Creating folders \e[0m"
+mkdir -p /config /defaults
+
+echo -e "\e[32m[INFO] Creating user \e[0m"
+useradd -u 1000 -U -d /config -s /sbin/nologin user
